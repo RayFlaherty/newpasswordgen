e548694b16d4b0873e6854914a52719447731514
RayFlaherty <90649331+RayFlaherty@users.noreply.github.com>
2024-01-01 13:25:54 -0600

    figuring out the functionality of the user interface

---
+++ script.js
@@ -0,0 +1,9 @@
+const letters = ["a", "b", "c", "d", "e", "f", "g"];
+const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+const symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+
+//document.querySelector("button").addEventListener("click", () => {
+// [...document.querySelectorAll('input[type="radio"]')].forEach(
+// (input) => (input.checked = false)
+//);
+//});
