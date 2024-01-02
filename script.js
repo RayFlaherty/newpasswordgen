dca46bafc5d2d4dc465fd3afe877b15d3c6592fa
RayFlaherty <90649331+RayFlaherty@users.noreply.github.com>
2024-01-02 10:36:55 -0600

    refractored and fixed passChars issued

--- script.js
+++ script.js
@@ -1,62 +1,64 @@
-const upperCase = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
-const lowerCase = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
+// const upperCase = [
+//   "A",
+//   "B",
+//   "C",
+//   "D",
+//   "E",
+//   "F",
+//   "G",
+//   "H",
+//   "I",
+//   "J",
+//   "K",
+//   "L",
+//   "M",
+//   "N",
+//   "O",
+//   "P",
+//   "Q",
+//   "R",
+//   "S",
+//   "T",
+//   "U",
+//   "V",
+//   "W",
+//   "X",
+//   "Y",
+//   "Z",
+// ];
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// const lowerCase = [
+//   "a",
+//   "b",
+//   "c",
+//   "d",
+//   "e",
+//   "f",
+//   "g",
+//   "h",
+//   "i",
+//   "j",
+//   "k",
+//   "l",
+//   "m",
+//   "n",
+//   "o",
+//   "p",
+//   "q",
+//   "r",
+//   "s",
+//   "t",
+//   "u",
+//   "v",
+//   "w",
+//   "x",
+//   "y",
+//   "z",
+// ];
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 const symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
-var length;
+var passLength, chars, password;
 
 function getLength() {
   var eightchar = document.getElementById("eightchar");
@@ -66,26 +68,46 @@ function getLength() {
   var twelvechar = document.getElementById("twelvechar");
 
   if (eightchar.checked == true) {
-    length = Number(eightchar.value);
-    return length;
+    passLength = Number(eightchar.value);
+    return passLength;
   } else if (ninechar.checked == true) {
-    length = Number(ninechar.value);
-    return length;
+    passLength = Number(ninechar.value);
+    return passLength;
   } else if (tenchar.checked == true) {
-    length = Number(tenchar.value);
-    return length;
+    passLength = Number(tenchar.value);
+    return passLength;
   } else if (elevenchar.checked == true) {
-    length = Number(elevenchar.value);
-    return length;
+    passLength = Number(elevenchar.value);
+    return passLength;
   } else if (twelvechar.checked == true) {
-    length = Number(twelvechar.value);
-    console.log(length);
+    passLength = Number(twelvechar.value);
+    console.log(passLength);
+    return passLength;
   } else console.log("no value selected");
 }
 
-function makePass() {
-  getLength();
-  console.log(length);
+function getChars() {
+  passLength = getLength();
+  const uCase = document.getElementById("upperCase").checked;
+  const lCase = document.getElementById("lowerCase").checked;
+  const nums = document.getElementById("numbers").checked;
+  const symb = document.getElementById("symbols").checked;
+  password = generatePass(uCase, lCase, nums, symb);
+
+  function generatePass(lCase, uCase, nums, symb) {
+    let chars = "";
+    if (lCase) chars = chars.concat(lowerCase);
+    if (uCase) chars = chars.concat(upperCase);
+    if (nums) chars = chars.concat(numbers);
+    if (symb) chars = chars.concat(symbols);
+
+    const passChars = [];
+    for (let i = 0; i < passLength; i++) {
+      const characters = chars[Math.floor(Math.random() * chars.length)];
+      passChars.push(String(characters));
+    }
+    console.log(passChars);
+  }
 }
 
 //document.querySelector("button").addEventListener("click", () => {
@@ -95,5 +117,6 @@ function makePass() {
 //});
 
 document.getElementById("gogo").addEventListener("click", () => {
-  makePass();
+  //makePass()
+  getChars();
 });
